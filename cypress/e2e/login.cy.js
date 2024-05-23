c4965da6c6e59243a80ff2df01d0260940e58dc8
Artem Gritsaenko <artem.gritsaenko@yarrl.pl>
2024-05-23 16:06:54 +0200

    Wrong user, mate

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,13 +2,13 @@ describe('login', () => {
   it('login successful', () => {
     cy.visit('/');
 
-    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="username"]').type('standard_user123');
     cy.get('[data-test="password"]').type('secret_sauce');
     cy.get('[data-test="login-button"]').click();
 
     cy.url().should('eq', 'https://www.saucedemo.com/inventory.html');
   });
-  it.only('error message for wrong credentials', () => {
+  it('error message for wrong credentials', () => {
     cy.visit('/');
 
     cy.get('[data-test="username"]').type('standard_user123');
