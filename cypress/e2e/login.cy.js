7543067426c4394570c8ebc43492529df511a235
Artem Gritsaenko <artem.gritsaenko@yarrl.pl>
2024-05-23 15:51:02 +0200

    eslint fixes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,20 +1,20 @@
 describe('login', () => {
   it('login successful', () => {
-    cy.visit('/')
+    cy.visit('/');
 
-    cy.get('[data-test="username"]').type('standard_user')
-    cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
 
-    cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
-  })
+    cy.url().should('eq', 'https://www.saucedemo.com/inventory.html');
+  });
   it.only('error message for wrong credentials', () => {
-    cy.visit('/')
+    cy.visit('/');
 
-    cy.get('[data-test="username"]').type('standard_user123')
-    cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="username"]').type('standard_user123');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
 
-    cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username and password do not match any user in this service')
-  })
-})
+    cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username and password do not match any user in this service');
+  });
+});
